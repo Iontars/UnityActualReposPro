0c18b4ea601e038c5d600500d251f26eb638c279
Граница миров <83580723+Iontars@users.noreply.github.com>
2025-04-14 11:02:21 +0700

    1

--- UI/Assets/Scripts/Main/Constants.cs
+++ UI/Assets/Scripts/Main/Constants.cs
@@ -7,5 +7,5 @@ public static class Constants
     public const string tagRightTimerValue = "RightTimerValue";
     public const string tagLeftTimerValue = "LeftTimerValue";
     public const int rightTimerValue = 59;
-    public const int leftTimerValue = 12;
+    public const int leftTimerValue = 23;
 }
